3ded810f0cd3a601ae111a999d7cb5489ce8aa6a
prerak <prerakgarg@ufl.edu>
2018-03-03 05:38:34 +0530

    Added question 1 and comments

--- hw2.py
+++ hw2.py
@@ -2,6 +2,29 @@ import numpy as np
 import astropy.units as u
 from matplotlib import pyplot as plt
 
+
+################Question1######################
+
+r = 100 * u.micron
+s = 1 * u.km
+tau = (2./3.)   # Assumed tau to be 2/3
+
+#1a
+n = (tau/(np.pi*(r**2)*s)).decompose().to(u.m**-3)
+print "Density of particles = ",n
+
+#1b
+mass_particle = 6.67e-4 * u.g        #Mass of one particle
+area = 1.3e6 * u.km**2               #Area of great plains
+height = 1 * u.km
+vol = area*height
+particles = vol*n
+mass_soil = (particles*mass_particle).decompose()
+print "Mass of soil lost = ",mass_soil
+
+
+###########Question 2#################
+
 # Assumed Values
 n = 1*u.cm**-3
 D = 100*u.pc
@@ -13,15 +36,18 @@ fwhm = 2
 freq = np.arange(10, 20, 0.1)
 
 # Part a
+# Calculates Tau
 def find_tau(sigma, s):
     return (n*sigma*s).decompose()
 
 
 # Part b
+# Equation of radiative transfer
 def Inu(I_0,tau,Snu):
     return I_0*np.exp(-tau) + Snu*(1-np.exp(-tau))
 
 
+# Iterates over the whole column to get the final output intensity
 def get_Inu(tau,I0):
     I = []
     del_tau = tau/100
@@ -31,24 +57,25 @@ def get_Inu(tau,I0):
     return I[100]
 
 # Part c
+# This function defines the cross section as a Gaussian in frequency space
 def sigma_nu_gauss(max):
     return max*np.exp((-4*np.log(2)*(freq-15)**2)/fwhm)*u.m**2
 
 
 #Part d
-# i.
+# i. Tau >>> 1
 tau = find_tau(sigma_nu_gauss(2e-4), del_s)
-print tau
 Intensity = []
 for tau_nu in tau:
     Intensity.append(get_Inu(tau_nu,I0))
 plt.plot(Intensity)
 plt.xlabel("Frequency")
 plt.ylabel("Intensity")
+plt.title("Tau >>> 1")
 plt.show()
 
 
-# ii
+# ii I0 = 0 and Tau < 1
 tau = find_tau(sigma_nu_gauss(2e-20), del_s)
 Intensity = []
 I0 = 0 * u.erg*u.s**-1*u.cm**-2*u.sr**-1*u.Hz**-1
@@ -57,21 +84,22 @@ for tau_nu in tau:
 plt.plot(Intensity)
 plt.xlabel("Frequency")
 plt.ylabel("Intensity")
+plt.title("I0 = 0 and Tau < 1")
 plt.show()
 
-#iii
+#iii I0 < Snu and Tau < 1
 tau = find_tau(sigma_nu_gauss(2e-22), del_s)
 Intensity = []
-print tau
 I0 = 0.05e-9 * u.erg*u.s**-1*u.cm**-2*u.sr**-1*u.Hz**-1
 for tau_nu in tau:
     Intensity.append(get_Inu(tau_nu,I0))
 plt.plot(Intensity)
 plt.xlabel("Frequency")
 plt.ylabel("Intensity")
+plt.title("I0 < Snu and Tau < 1")
 plt.show()
 
-#iv
+#iv I0 > Snu and Tau < 1
 tau = find_tau(sigma_nu_gauss(2e-22), del_s)
 Intensity = []
 I0 = 1e-9 * u.erg*u.s**-1*u.cm**-2*u.sr**-1*u.Hz**-1
@@ -80,4 +108,5 @@ for tau_nu in tau:
 plt.plot(Intensity)
 plt.xlabel("Frequency")
 plt.ylabel("Intensity")
+plt.title("I0 > Snu and Tau < 1")
 plt.show()
